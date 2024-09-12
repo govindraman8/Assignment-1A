f5401ef587004a0510d3c98c553f3b8c1d34c29d
Govind Raman <govindraman@outlook.com>
2024-09-12 14:05:10 -0400

    First Commit

--- write_ids.c
+++ write_ids.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    char GITHUB_ID[] = "Fill_in_Your_Github_Id";
-    char STUDENT_ID[] = "Fill_in_Your_Student_Id";
+    char GITHUB_ID[] = "govindraman8";
+    char STUDENT_ID[] = "gbr5153";
 
     return 0 ;
 }
